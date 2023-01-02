b2eb14855f3089cceca3af252730330ebf551239
unknown <threezinedine@gmail.com>
2023-01-02 16:27:29 +0700

    add the user_controller to the test

--- tests/api/v1/station_test.py
+++ tests/api/v1/station_test.py
@@ -4,16 +4,18 @@ from fastapi.testclient import TestClient
 
 from main import app
 from database.connection import get_db
+from app.controllers import UserController
 
 
 class StationTest(unittest.TestCase):
     def setUp(self):
         self.test_client = TestClient(app)
+        self.user_controller = UserController(next(get_db()))
 
     def tearDown(self):
         pass
 
-    def test_given_when_no_user_is_created_when_a_new_user_is_registered_then_that_user_should_be_created(self):
+    def test_given_when_no_user_is_created_when_a_new_user_is_registered_then_returns_the_200_response_with_user_info(self):
         response = self.test_client.post(
                     "/users/register",
                     json={
@@ -29,3 +31,12 @@ class StationTest(unittest.TestCase):
                 "userId": 1,
                 "username": "threezinedine"
             })
+
+    def test_given_when_no_user_is_created_when_a_new_user_is_registered_then_that_user_should_be_created(self):
+        self.test_client.post(
+                    "/users/register",
+                    json={
+                        "username": "threezinedine",
+                        "password": "threezinedine"
+                    }
+                )
