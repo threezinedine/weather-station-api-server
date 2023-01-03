4a083194ff12be86ce760ea88fc029b21bbb4c52
unknown <threezinedine@gmail.com>
2023-01-03 19:22:44 +0700

    add test for creating a new user

--- tests/controllers/user_controller_test.py
+++ tests/controllers/user_controller_test.py
@@ -16,3 +16,11 @@ class UserControllerTest(unittest.TestCase):
     def test_given_no_user_is_created_when_controller_get_a_user_by_name_then_receives_None(self):
         user = self.user_controller.get_user_by_name(username="threezinedine")
         assert user is None
+
+    def test_given_no_user_is_created_when_create_a_new_user_then_that_user_is_created(self):
+        self.user_controller.create_new_user(username="threezinedine", password="threezinedine")
+
+        user = self.user_controller.get_user_by_name(username="threezinedine")
+
+        assert user.username == "threezinedine"
+        assert user.is_match("threezinedine")
