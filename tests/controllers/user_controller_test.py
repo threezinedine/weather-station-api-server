3c446c61438594c4d30168db7c30987cd0c68b28
unknown <threezinedine@gmail.com>
2023-01-05 18:14:00 +0700

    refactor code for user_controller_testing

--- tests/controllers/user_controller_test.py
+++ tests/controllers/user_controller_test.py
@@ -8,6 +8,9 @@ from tests import get_testing_session
 
 
 class UserControllerTest(unittest.TestCase):
+    testing_username = "threezinedine"
+    testing_password = "threezinedine1"
+
     def setUp(self):
         self.session = next(get_testing_session())
         self.user_controller = UserController(self.session)
@@ -18,13 +21,13 @@ class UserControllerTest(unittest.TestCase):
         self.session.close()
 
     def test_given_no_user_is_created_when_controller_get_a_user_by_name_then_receives_None(self):
-        user = self.user_controller.get_user_by_name(username="threezinedine")
+        user = self.user_controller.get_user_by_name(username=self.testing_username)
         assert user is None
 
     def test_given_no_user_is_created_when_create_a_new_user_then_that_user_is_created(self):
-        self.user_controller.create_new_user(username="threezinedine", password="threezinedine")
+        self.user_controller.create_new_user(username=self.testing_username, password=self.testing_password)
 
-        user = self.user_controller.get_user_by_name(username="threezinedine")
+        user = self.user_controller.get_user_by_name(username=self.testing_username)
 
-        assert user.username == "threezinedine"
-        assert user.is_match("threezinedine")
+        assert user.username == self.testing_username
+        assert user.is_match(self.testing_password)
