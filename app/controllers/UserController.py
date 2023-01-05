30c695d1b3ce722cd935c8991069f88d60de8957
unknown <threezinedine@gmail.com>
2023-01-05 18:34:13 +0700

    pass test for creating user with existed username

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -14,11 +14,18 @@ class UserController:
     def get_user_by_name(self, username: str) -> Union[User, None]:
         return self.session.query(User).filter(User.username == username).first()
 
-    def create_new_user(self, username: str, password: str) -> None:
-        user = User(username=username, password=password)
+    def create_new_user(self, username: str, password: str) -> bool:
+        result = False
 
-        self.session.add(user)
-        self.session.commit()
+        user = self.get_user_by_name(username=username)
+
+        if user is None:
+            new_user = User(username=username, password=password)
+            self.session.add(new_user)
+            self.session.commit()
+            result = True
+
+        return result
 
     def get_all_users(self) -> List[User]:
         return self.session.query(User).all()
