61c382f488a0a2022a037e161b695c98bb328686
unknown <threezinedine@gmail.com>
2023-01-07 22:38:17 +0700

    pass test for querying all stations when no stations are not created

--- app/controllers/StationController.py
+++ app/controllers/StationController.py
@@ -1,8 +1,26 @@
+from typing import (
+    Dict,
+    Union,
+    List,
+    Tuple,
+)
 from sqlalchemy.orm import (
     Session,
+)
+
+from database.models import (
+    Station,
+)
+from app.exceptions import (
+    HTTP_200_OK,
+    DETAIL_KEY,
+    STATUS_CODE_KEY,
 )
 
 
 class StationController:
     def __init__(self, session: Session):
         self.session = session
+
+    def get_all_stations(self) -> Tuple[Dict[str, Union[int, Union[str, None]]], List[Station]]:
+        return {STATUS_CODE_KEY: HTTP_200_OK, DETAIL_KEY: None}, []
