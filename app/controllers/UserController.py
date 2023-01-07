733bd248bc3648956b6b6cacf6fbae914b09925f
unknown <threezinedine@gmail.com>
2023-01-07 12:51:07 +0700

    refactoring for creating new user when no users are created

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from typing import (
     Union,
     List,
+    Tuple,
+    Dict,
 )
 
 from database.models import User
@@ -17,7 +19,7 @@ class UserController:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_user_by_name(self, username: str) -> Union[User, None]:
+    def get_user_by_name(self, username: str) -> Tuple[Dict[str, Union[int, str]], User]:
         status = {}
         user = self.session.query(User).filter(User.username == username).first()
 
@@ -29,7 +31,7 @@ class UserController:
     def get_user_by_id(self, userId:int) -> Union[User, None]:
         return self.session.query(User).filter(User.userId == userId).first()
 
-    def create_new_user(self, username: str, password: str) -> bool:
+    def create_new_user(self, username: str, password: str) -> Tuple[Dict[str, Union[int, str]], User]:
         status = {}
         user = User(username, password)
 
