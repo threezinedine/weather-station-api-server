606e286ca4db4219b5c9589f89616456df448852
unknown <threezinedine@gmail.com>
2023-01-07 15:05:40 +0700

    pass test for querying the users by userid

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -35,8 +35,8 @@ class UserController:
 
         return status, user
 
-    def get_user_by_id(self, userId:int) -> Union[User, None]:
-        return self.session.query(User).filter(User.userId == userId).first()
+    def get_user_by_id(self, userId:int) -> Tuple[Dict[str, Union[int, Union[str, None]]], User]:
+        return {STATUS_CODE_KEY: HTTP_200_OK, DETAIL_KEY: None}, self.session.query(User).filter(User.userId == userId).first()
 
     def create_new_user(self, username: str, password: str) -> Tuple[Dict[str, Union[int, Union[str, None]]], User]:
         status = {}
