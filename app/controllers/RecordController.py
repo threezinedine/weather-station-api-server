462115092e6d999f2de78eafa7f6f3949bb02874
unknown <threezinedine@gmail.com>
2023-01-08 18:40:00 +0700

    pass test for get all records when no record is created

--- app/controllers/RecordController.py
+++ app/controllers/RecordController.py
@@ -1,6 +1,23 @@
 from sqlalchemy.orm import Session
+from typing import (
+    List, 
+    Dict,
+    Union,
+    Tuple,
+)
+
+from database.models import (
+    Record,
+)
+
+from app.exceptions import (
+    OK_STATUS,
+)
 
 
 class RecordController:
     def __init__(self, session: Session):
         self.session = session
+
+    def get_all_records(self) -> Tuple[Dict[str, Union[int, Union[str, None]]], Record]:
+        return OK_STATUS, self.session.query(Record).all()
