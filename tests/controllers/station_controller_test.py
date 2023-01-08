ec43d30483a961a9141dbaaf9f125c890835a1ab
unknown <threezinedine@gmail.com>
2023-01-08 11:33:04 +0700

    add test for chaning the push time intervale

--- tests/controllers/station_controller_test.py
+++ tests/controllers/station_controller_test.py
@@ -165,15 +165,27 @@ class StationControllerTest(unittest.TestCase):
     def test_given_a_station_user_are_created_when_create_the_relationship_with_non_existed_username_then_returns_user_does_not_exist_and_none(self):
         creataAStationAndAnUserBy(self.user_controller, self.station_controller)
 
-        status, stations = self.station_controller.add_username(username=FIRST_TEST_USER_WRONG_USERNAME, stationName=TEST_STATION_NAME)
+        status, station = self.station_controller.add_username(username=FIRST_TEST_USER_WRONG_USERNAME, stationName=TEST_STATION_NAME)
 
         assertStatus(status, USERNAME_DOES_NOT_EXISTED_STATUS_CODE, USERNAME_DOES_NOT_EXISTED_DETAIL)
-        assert stations is None
+        assert station is None
 
     def test_given_a_station_user_are_created_when_create_the_relationship_with_non_existed_station_name_then_returns_station_does_not_exist_and_none(self):
         creataAStationAndAnUserBy(self.user_controller, self.station_controller)
 
-        status, stations = self.station_controller.add_username(username=FIRST_TEST_USER_USERNAME, stationName=TEST_STATION_WRONG_STATION_NAME)
+        status, station = self.station_controller.add_username(username=FIRST_TEST_USER_USERNAME, stationName=TEST_STATION_WRONG_STATION_NAME)
 
         assertStatus(status, STATION_DOES_NOT_EXIST_STATUS_CODE, STATION_DOES_NOT_EXIST_DETAIL)
-        assert stations is None
+        assert station is None
+
+    def test_given_a_station_is_created_when_changing_the_pushing_interval_with_existed_station_then_returns_ok_and_that_station(self):
+        createAStationBy(self.station_controller)
+
+        status, station = self.station_controller.change_pushing_time_interval(stationName=TEST_STATION_NAME, new_pushingDataIntervalInSeconds=10)
+        
+        assertStatus(status, HTTP_200_OK)
+        assertStaion(station, TEST_STATION_NAME, TEST_STATION_POSITION, 10)
+
+        _, station = self.station_controller.get_station_by_station_name(TEST_STATION_NAME)
+        assertStation(station, TEST_STATION_NAME, TEST_STATION_POSITION, 10)
+
