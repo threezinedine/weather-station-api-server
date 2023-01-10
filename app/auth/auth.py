b6132c8cf8ae3dbd2128c4b8c27ca1bbf6e23280
unknown <threezinedine@gmail.com>
2023-01-10 23:49:50 +0700

    update generate token to have the expired time

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -22,7 +22,7 @@ auth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 def generate_token(payload: Dict[str, str], expired_delta: timedelta = timedelta(minutes=15)) -> str:
     expire_time = datetime.utcnow() + expired_delta
-    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    token = jwt.encode(dict(exp=expire_time, **payload), secret_key, algorithm="HS256")
     return token
 
 def verify_token(token: str = Depends(auth2_scheme)) -> Dict[str, str]:
