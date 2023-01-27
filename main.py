cf443c89ee15ed693c9a69a39454562b8d41b195
unknown <threezinedine@gmail.com>
2023-01-27 16:09:47 +0700

    refactorign the code for the login page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +10,22 @@ from app.api.v1 import (
 )
 
 application = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 application.include_router(users.router)
 application.include_router(stations.router)
